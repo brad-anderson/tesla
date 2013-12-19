41d0817e5c05b9c0e10b1a25f677e36ac939532c
Brad Anderson <eco@gnuk.net>
2013-12-18 20:04:29 -0700

    Some extra changes made on deployment

--- tesla.d
+++ tesla.d
@@ -4,12 +4,14 @@ import std.regex;
 import std.datetime;
 import std.net.curl;
 import std.xml;
+import std.utf;
 
 enum irc_regex = ctRegex!(r"^(?:[:@]([^\s]+) )?([^\s]+)(?: ((?:[^:\s][^\s]* ?)*))?(?: ?:(.*))?$");
 //enum url_regex = ctRegex!(r"(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?", "gi");
-enum url_regex = ctRegex!(r"^(https?|ftp)://[^\s/$.?#].[^\s]*$", "gi");
+enum url_regex = ctRegex!(r"(https?|ftp)://[^\s/$.?#].[^\s]*", "gi");
 enum title_regex = ctRegex!(r"<title>(.*?)<", "si");
 enum extra_whitespace_regex = ctRegex!(r"\s{2,}", "g");
+enum refuse_topic_regex = ctRegex!(r"^[@!]topic", "gi");
 
 class IrcBot
 {
@@ -59,72 +61,84 @@ class IrcBot
 
     void process()
     {
-        string line = m_stream.readLine().idup;
+	try
+	{
+		string line = m_stream.readLine().idup;
 
-        auto c = match(line, irc_regex).front;
-        auto source     = c[1];
-        auto command    = c[2];
-        auto target     = c[3];
-        auto parameters = c[4];
+		auto c = match(line, irc_regex).front;
+		auto source     = c[1];
+		auto command    = c[2];
+		auto target     = c[3];
+		auto parameters = c[4];
 
-        //writeln(line);
-        writefln("---");
-        writefln(" - time:       %s", Clock.currTime().toSimpleString);
-        writefln(" - source:     %s", source);
-        writefln(" - command:    %s", command);
-        writefln(" - target:     %s", target);
-        writefln(" - parameters: %s", parameters);
+		//writeln(line);
+		writefln("---");
+		writefln(" - time:       %s", Clock.currTime().toSimpleString);
+		writefln(" - source:     %s", source);
+		writefln(" - command:    %s", command);
+		writefln(" - target:     %s", target);
+		writefln(" - parameters: %s", parameters);
 
-        if(command == "PING")
-        {
-            writeln("PONG");
-            m_stream.writeLine(format("PONG %s", parameters));
-        }
+		if(command == "PING")
+		{
+		    writeln("PONG");
+		    m_stream.writeLine(format("PONG %s", parameters));
+		}
 
-        if(command == "PRIVMSG")
-        {
-            auto urls = array(map!(a => a.captures[0])(match(parameters, url_regex)));
-            if(urls.length > 0)
-            {
-                foreach(url; urls)
-                {
-                    try
-                    {
-                        string contents = get(url).idup;
-                        auto title_match = match(contents, title_regex);
-                        if(title_match)
-                        {
-                            auto title =
-                                decode( // decode xml entities
-                                    replace( // remove chains of two or more whitspace
-                                        replace( // remove newlines
-                                            strip(title_match.captures[1]), // remove whitespace padding
-                                                "\n", ""), extra_whitespace_regex, " "));
-                            
-                            // Handle a few common entities the xml decoding
-                            // won't catch
-                            title = replace(title, "&laquo;", "\u00ab");
-                            title = replace(title, "&raquo;", "\u00bb");
-                            title = replace(title, "&copy;", "\u00a9");
-                            title = replace(title, "&nbsp;", " ");
-                            title = replace(title, "&reg;", "\u00ae");
-                            title = replace(title, "&trade;", "\u2122");
+		if(command == "PRIVMSG")
+		{
+		    if(match(parameters, refuse_topic_regex))
+		    {
+			channelMessage(target, "No.");
+		    }
+		    auto urls = array(map!(a => a.captures[0])(match(parameters, url_regex)));
+		    if(urls.length > 0)
+		    {
+			foreach(url; urls)
+			{
+			    try
+			    {
+				string contents = get(url).idup;
+				auto title_match = match(contents, title_regex);
+				if(title_match)
+				{
+				    auto title =
+					decode( // decode xml entities
+					    replace( // remove chains of two or more whitspace
+						replace( // remove newlines
+						    strip(title_match.captures[1]), // remove whitespace padding
+							"\n", ""), extra_whitespace_regex, " "));
+				    
+				    // Handle a few common entities the xml decoding
+				    // won't catch
+				    title = replace(title, "&laquo;", "\u00ab");
+				    title = replace(title, "&raquo;", "\u00bb");
+				    title = replace(title, "&copy;", "\u00a9");
+				    title = replace(title, "&nbsp;", " ");
+				    title = replace(title, "&reg;", "\u00ae");
+				    title = replace(title, "&trade;", "\u2122");
 
-                            channelAction(target, format("\x0314%s", title));
-                        }
-                        else
-                        {
-                            writefln("** Could not retrieve title **", url);
-                        }
+				    //channelAction(target, format("\x0314%s", title));
+				    channelAction(target, format("[ %s ]", title));
+				}
+				else
+				{
+				    writefln("** Could not retrieve title **", url);
+				}
 
-                    }
-                    catch(CurlException e)
-                    {
-                        writefln("** Could not retrieve %s **", url);
-                    }
-                }
-            }
-        }
+			    }
+			    catch(CurlException e)
+			    {
+				writefln("** Could not retrieve %s **", url);
+			    }
+			}
+		    }
+		}
+	}
+	catch(UTFException e)
+	{
+		writeln("Invalid UTF character");
+	}
     }
 
     void channelMessage(string channel, string message)
@@ -166,6 +180,13 @@ void main()
 
     while(bot.connected())
     {
-        bot.process();
+	try
+	{
+        	bot.process();
+	}
+	catch(Exception e)
+	{
+		writeln("Some exception happend...recklessly ignoring");
+	}
     }
 }
